775da92694bc4938cdd1c8c01f1ae0b4061327e8
Sasan Rafat Nami <Nami.srn@gmail.com>
2023-06-04 12:50:54 +0200

    Update MailMate.ino
    kleine änderungen bei der Entfernung und Delay.
    22 cm statt 30 cm
    1000 ms statt 500 ms

--- MailMate.ino
+++ MailMate.ino
@@ -1,48 +1,48 @@
-#include <Servo.h> 
+#include <Servo.h> // Die Servo-Bibliothek wird inkludiert, die zur Steuerung des Servomotors benötigt wird.
 
-#define TRIG_PIN 2 // Definiere den Pin für das Trig-Signal des Ultraschallsensors.
-#define ECHO_PIN 4 // Definiere den Pin für das Echo-Signal des Ultraschallsensors.
+#define TRIG_PIN 2 // Der Pin für das Trig-Signal des Ultraschallsensors wird definiert.
+#define ECHO_PIN 4 // Der Pin für das Echo-Signal des Ultraschallsensors wird definiert.
 
-Servo servoMotor; // Erzeuge ein Servo-Objekt, das wir verwenden, um den Servomotor zu steuern.
+Servo servoMotor; // Ein Servo-Objekt wird erstellt, das zur Steuerung des Servomotors verwendet wird.
 
-bool isServoTurned = false; // Eine boolesche Variable, die speichert, ob der Servomotor gedreht wurde. Anfangs ist der Wert 'false'.()
+bool isServoTurned = false; // Eine boolesche Variable wird initialisiert, die speichert, ob der Servomotor gedreht wurde. Der Anfangswert ist 'false'.
 
 void setup() {
-  pinMode(TRIG_PIN, OUTPUT); // Den Trig-Pin als Ausgang setzen.
-  pinMode(ECHO_PIN, INPUT); // Den Echo-Pin als Eingang setzen.
-  Serial.begin(9600); 
+  pinMode(TRIG_PIN, OUTPUT); // Der Trig-Pin wird als Ausgang festgelegt.
+  pinMode(ECHO_PIN, INPUT); // Der Echo-Pin wird als Eingang festgelegt.
+  Serial.begin(9600); // Die serielle Kommunikation wird mit einer Baudrate von 9600 begonnen.
   
-  servoMotor.attach(9); // Das Servo-Objekt mit dem Pin 9 verbinden.
+  servoMotor.attach(9); // Das Servo-Objekt wird mit dem Pin verbunden, an den der Servomotor angeschlossen ist. In diesem Fall ist das Pin 9.
 }
 
 void loop() {
-  digitalWrite(TRIG_PIN, LOW); // Das Trig-Signal zunächst auf LOW setzen
-  delayMicroseconds(2); // Warte 2 Mikrosekunden.
-  digitalWrite(TRIG_PIN, HIGH); // Setze das Trig-Signal auf HIGH...
-  delayMicroseconds(10); // ...halte es für 10 Mikrosekunden auf HIGH...
-  digitalWrite(TRIG_PIN, LOW); // ...und setze es dann wieder auf LOW. Das sendet den Ultraschallimpuls aus.
+  digitalWrite(TRIG_PIN, LOW); // Das Trig-Signal wird zunächst auf LOW gesetzt.
+  delayMicroseconds(2); // Es wird 2 Mikrosekunden gewartet.
+  digitalWrite(TRIG_PIN, HIGH); // Das Trig-Signal wird auf HIGH gesetzt...
+  delayMicroseconds(10); // ...für 10 Mikrosekunden auf HIGH gehalten...
+  digitalWrite(TRIG_PIN, LOW); // ...und dann wieder auf LOW gesetzt. Das sendet den Ultraschallimpuls aus.
   
-  long duration = pulseIn(ECHO_PIN, HIGH); // Messe die Dauer, wie lange das Echo-Signal HIGH ist. Das ist die Zeit, die der Ultraschallimpuls für die Hin- und Rückreise benötigt.
+  long duration = pulseIn(ECHO_PIN, HIGH); // Die Dauer, wie lange das Echo-Signal HIGH ist, wird gemessen. Das ist die Zeit, die der Ultraschallimpuls für die Hin- und Rückreise benötigt.
   
-  float distance = (duration/2) / 29.1; // Berechne die Entfernung in cm. Wir teilen die Dauer durch 2 (weil die Zeit für Hin- und Rückreise gemessen wurde) und teilen dann durch 29.1 um die Einheit in Zentimeter zu konvertieren.
+  int distance = (duration/2) / 29.1; // Die Entfernung in cm wird berechnet. Die Dauer wird durch 2 geteilt (weil die Zeit für Hin- und Rückreise gemessen wurde) und dann durch 29.1 geteilt, um die Einheit in Zentimeter zu konvertieren.
   
-  Serial.print("Entfernung: "); // Gebe "Entfernung: " aus...
+  Serial.print("Entfernung: "); // "Entfernung: " wird ausgegeben...
   Serial.print(distance); // ...gefolgt von der gemessenen Entfernung...
-  Serial.println(" cm"); // ...und " cm" am Ende, um anzugeben, dass die Entfernung in Zentimeter ist.
+  Serial.println(" cm"); // ...und " cm" am Ende, um anzugeben, dass die Entfernung in Zentimetern ist.
 
-  // Wenn die gemessene Entfernung kleiner als 30 cm ist und der Servo noch nicht gedreht wurde, drehen wir den Servomotor.
-  if (distance < 24 && !isServoTurned) {
-    servoMotor.write(180); // Drehen den Servomotor um 180 Grad.
-    isServoTurned = true; // Setzen die Variable 'isServoTurned' auf true, da wir den Servomotor jetzt gedreht haben.
-    delay(1000); // Warten eine Sekunde, um sicherzustellen, dass der Servomotor Zeit zum Drehen hat.
+  // Wenn die gemessene Entfernung kleiner als 30 cm ist und der Servomotor noch nicht gedreht wurde, wird der Servomotor gedreht.
+  if (distance < 22 && !isServoTurned) {
+    servoMotor.write(180); // Der Servomotor wird um 180 Grad gedreht.
+    isServoTurned = true; // Die Variable 'isServoTurned' wird auf true gesetzt, da der Servomotor nun gedreht ist.
+    delay(1000); // Es wird eine Sekunde gewartet, um sicherzustellen, dass der Servomotor genügend Zeit zum Drehen hat.
   }
   
-  // Wenn die gemessene Entfernung größer oder gleich 30 cm ist und der Servomotor bereits gedreht wurde, drehen wir ihn zurück auf 0 Grad.
-  if (distance >= 24 && isServoTurned) {
-    servoMotor.write(0); // Drehen den Servomotor auf 0 Grad zurück.
-    isServoTurned = false; // Setzen die Variable 'isServoTurned' zurück auf false, da der Servomotor nun nicht mehr gedreht ist.
-    delay(1000); // Warten eine Sekunde, um sicherzustellen, dass der Servomotor Zeit zum Drehen hat.
+  // Wenn die gemessene Entfernung größer oder gleich 22 cm ist und der Servomotor bereits gedreht wurde, wird er zurück auf 0 Grad gedreht.
+  if (distance >= 22 && isServoTurned) {
+    servoMotor.write(0); // Der Servomotor wird auf 0 Grad zurückgedreht.
+    isServoTurned = false; // Die Variable 'isServoTurned' wird auf false gesetzt, da der Servomotor nun nicht mehr gedreht ist.
+    delay(1000); // Es wird eine Sekunde gewartet, um sicherzustellen, dass der Servomotor genügend Zeit zum Drehen hat.
   }
   
-  delay(500); // Warten 0,5 Sekunden bevor wir die Schleife erneut starten.
+  delay(1000); // Es wird eine Sekunde gewartet, bevor die Schleife erneut gestartet wird.
 }
