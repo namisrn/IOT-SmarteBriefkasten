2d43c3c7e5c636df6a1b0af0ad71e2da12860d29
Sasan Rafat Nami <Nami.srn@gmail.com>
2023-07-06 11:45:56 +0200

    Update MailMate-Final-OhneLoRa-MitStepper.ino
    USS-Abstand auf 11 cm verringert.

--- MailMate-Final-OhneLoRa-MitStepper.ino
+++ MailMate-Final-OhneLoRa-MitStepper.ino
@@ -1,4 +1,4 @@
-#include <Stepper.h>  // Bibliothek für die Steuerung von Schrittmotoren einbinden
+#include <Stepper.h>
 
 // Pins definieren
 #define TRIG_PIN 7  // Trig Pin für Ultraschallsensor
@@ -9,8 +9,8 @@
 // Stepper-Objekt erstellen. Pin 8, 9, 10 und 11 am Arduino sind mit den Eingangspins am L293DNE verbunden
 Stepper stepper(STEPS_PER_REV, 8, 9, 10, 11); 
 
-bool ledStatus = false; // Zustand der LED, standardmäßig ausgeschaltet
-bool motorStatus = false; // Zustand des Motors, standardmäßig in der Ausgangsposition
+bool ledStatus = false;
+bool motorStatus = false;
 
 void setup() {
   // Pins als Ausgang bzw. Eingang festlegen
@@ -36,34 +36,27 @@ void loop() {
   // Echo-Laufzeit messen
   long duration = pulseIn(ECHO_PIN, HIGH);
   
-  // Distanz berechnen (Formel: distanz = (Laufzeit/2) / 29.1)
+  // Distanz berechnen
   float distance = (duration/2) / 29.1;
 
-  // Überprüfen, ob ein Objekt innerhalb von 15 cm ist
-  if (distance < 15 && ledStatus == false) {
-    // LED einschalten
+  // Überprüfen, ob ein Objekt innerhalb von 10 cm ist
+  if (distance < 11 && ledStatus == false) {
     digitalWrite(LED_PIN, HIGH);
     ledStatus = true;
-    // Statusmeldung ausgeben
-    Serial.println("Objekt erkannt, Entfernung weniger als 15cm!");
-    // Motor drehen, wenn er noch nicht gedreht hat
+    Serial.println("Objekt erkannt, Entfernung weniger als 11cm!");
     if (!motorStatus) {
-      stepper.step(STEPS_PER_REV/2);  // Drehung um eine halbe Umdrehung
+      stepper.step(STEPS_PER_REV/2);
       motorStatus = true;
     }
-  } else if(distance >= 15 && ledStatus == true){
-    // LED ausschalten
+  } else if(distance >= 11 && ledStatus == true){
     digitalWrite(LED_PIN, LOW);
     ledStatus = false;
-    // Statusmeldung ausgeben
-    Serial.println("Kein Objekt innerhalb von 15cm erkannt.");
-    // Motor zurückdrehen, wenn er gedreht hat
+    Serial.println("Kein Objekt innerhalb von 11cm erkannt.");
     if (motorStatus) {
-      stepper.step(-STEPS_PER_REV/2);  // Drehung um eine halbe Umdrehung zurück
+      stepper.step(-STEPS_PER_REV/2);
       motorStatus = false;
     }
   }
 
-  // Verzögerung hinzufügen, um saubere Messungen zu gewährleisten
   delay(100);
 }
